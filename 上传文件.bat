333f02b723b138f0e3d106e3a95b9159866a4281
SMWHff <SMWHff@163.com>
2023-01-22 16:45:05 +0800

    MUMU模拟器VT提示.png

--- 上传文件.bat
+++ 上传文件.bat
@@ -1,10 +1,11 @@
-@echo off
+
 set file_path=%1
 set file_name=%~nx1
-copy %file_path% "./img/%file_name%"
+xcopy "%file_path%" "%~dp0\img\" /s /c /d /y
 cd "E:\AppData\Roaming\GitHub\smos"
 git status
 git add .
 git commit -m "%file_name%"
 git push
+echo "http://cdn.jsdelivr.net/gh/SMWHff/SMOS@master/Img/%file_name%" | clip
 pause
